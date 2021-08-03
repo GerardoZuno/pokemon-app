5a614049bd9e0c993f41bfc7f97fe650271b6d33
GerardoZuno <gerardozunounitec@gmail.com>
2021-08-03 17:44:53 -0500

    Add login with google

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,12 @@ import {createStore, combineReducers, compose, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 
 import pokeReducer from './pokeDucks'
+import usuarioReducer from './userDucks'
 
 const rootReducer = combineReducers({
     pokemones: pokeReducer,
+    usuario: usuarioReducer
+
 })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
--- src/redux/userDucks.js
+++ src/redux/userDucks.js
@@ -1,47 +1,61 @@
+import { auth, firebase } from "../firebase";
 // data inicial
 
 const dataInicial = {
-    loading: false,
-    activo: false,
-}
+  loading: false,
+  activo: false,
+};
 
 //types
 
-const LOADING = 'LOADING'
-const USUARIO_ERROR = 'USUARIO_ERROR'
-
+const LOADING = "LOADING";
+const USUARIO_ERROR = "USUARIO_ERROR";
+const USUARIO_EXITO = "USUARIO_EXITO";
 
 //reducer
 
-export default function usuarioReducer(state = dataInicial, action){
-    switch(action.type){
+export default function usuarioReducer(state = dataInicial, action) {
+  switch (action.type) {
+    case LOADING:
+      return { ...state, loading: true };
 
-        case LOADING:
-            return {...state, loading: true}
+    case USUARIO_ERROR:
+      return { ...dataInicial };
+    case USUARIO_EXITO:
+      return { ...state, loading: false, user: action.payload };
 
-        case USUARIO_ERROR:
-            return {...dataInicial}
-
-        default: 
-                 return{...state}
-    }
+    default:
+      return { ...state };
+  }
 }
-
-
 
 //accions
 export const ingresoUsuarioAccion = () => async (dispatch, getState) => {
+  dispatch({
+    type: LOADING,
+  });
+  try {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const res = await auth.signInWithPopup(provider);
+    console.log(res);
     dispatch({
-        type: LOADING,
-        
-    })
-    try {
-
-    }catch(err){
-        console.log(err)
-        dispatch({
-           type: USUARIO_ERROR,
-        })
-    }
-}
-
+      type: USUARIO_EXITO,
+      payload: {
+        uid: res.user.uid,
+        email: res.user.email,
+      },
+    });
+    localStorage.setItem(
+      "usuario",
+      JSON.stringify({
+        uid: res.user.uid,
+        email: res.user.email,
+      })
+    );
+  } catch (err) {
+    console.log(err);
+    dispatch({
+      type: USUARIO_ERROR,
+    });
+  }
+};
