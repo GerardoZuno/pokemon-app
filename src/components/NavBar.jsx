116cb7965d990985de1d11a181b62437edd17c35
GerardoZuno <gerardozunounitec@gmail.com>
2021-08-03 16:47:51 -0500

    Add NavBAR

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link, NavLink } from "react-router-dom";
+
+const NavBar = () => {
+    return (
+        <div className="navbar navbar-dark bg-dark mb-5">
+            <Link to="/" className="navbar-brand">
+                App Pokemon
+            </Link>
+            <div className="d-flex">
+                <NavLink className="btn btn-dark mr-2" to="/" exact>
+                    Inicio
+                </NavLink>
+                <NavLink className="btn btn-dark mr-2" to="/login" exact>
+                    Login
+                </NavLink>
+                <button className="btn btn-dark mr-2" exact>Cerrar Sesion</button>
+
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
